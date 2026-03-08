3dddf486b301e22c469d0c5a2675bebfc4483f8d
Unimon <kemikksahurrr@gmail.com>
2026-03-08 21:50:16 +0700

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FarmCard from "../components/FarmCard";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Start Your Farm</h2>
+      <FarmCard />
+    </div>
+  );
+}
